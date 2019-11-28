aa53d9d377019c4e8b746fc2e4fd85ae0e7cbb33
ceesvrr <amberbrayboy@yahoo.com>
2019-11-27 22:51:19 -0500

    Implemented IO factory

---
+++ Studenttracking/IO/StudentManagerBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Studenttracking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Studenttracking.IO
+{
+    public static class StudentManagerBuilder
+    {
+
+        public static async Task<StudentManager> Build(StorageFile spreadSheet)
+        {
+            var lines = await FileIO.ReadLinesAsync(spreadSheet);
+            var lineNumber = 0;
+            var studentManager = new StudentManager();
+            return studentManager;
+        }
+    }
+}
